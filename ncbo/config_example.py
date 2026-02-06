6b29d562b85dbcdce317d1ab8c1606b3cb1d797b
cconrad8 <christina.parry@sagebase.org>
2026-02-05 21:51:30 -0500

    start fresh

--- ncbo/config_example.py
+++
@@ -1,2 +0,0 @@
-# Copy this file and save as config.py to your directory.  Add config.py to .gitignore so you don't commit the file.
-# API_KEY = ""
--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install global npm packages (if needed)
-npm install -g eslint
-
-# Install Python dependencies
-pip install -r requirements.txt
